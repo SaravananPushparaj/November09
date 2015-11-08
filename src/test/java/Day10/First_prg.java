0233caadeca751369fce47b6f27ab324966d7414
unknown <saravananpushparaj@gmail.com>
2015-11-08 15:57:32 +0530

    V1.0

---
+++ src/test/java/Day10/First_prg.java
@@ -0,0 +1,15 @@
+package Day10;
+
+import org.testng.annotations.Test;
+
+public class First_prg {
+	
+	@Test(groups={"smoke"})
+	public void display()
+	{
+		
+		System.out.println("Hello world");
+		
+	}
+
+}
